5c18e280d1d8b7b50f57aeda928d37a58b9f0e7a
Fabio Daniel <fdaniel.aborges@gmail.com>
2020-11-30 18:32:24 -0300

    refatorando rota usuario

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const routers = express.Router();
 
-/*
-routers.get('/',(req,resp)=>{
 
+routers.get('/index/:idUsuario',(req,resp)=>{
+    
     //resp.send('pagina inicial');
     //resp.sendFile(__dirname+'/front-end/index.html');
-    resp.render('telaInicial',{layout:'index'}); //ou resp.render('dadosIniciais',{layout:'nome layout onde vai ser impresso'});
+    resp.render('index',{layout:'main', name: req.params.idUsuario}); //ou resp.render('dadosIniciais',{layout:'nome layout onde vai ser impresso'});
 
 }
-);*/
+);
 
 routers.post('/',(req,resp)=>{
     console.log(req.body);
-    //resp.send('pagina inicial');
-    //resp.sendFile(__dirname+'/front-end/index.html');
-    resp.render('index',{layout:'main'}); //ou resp.render('dadosIniciais',{layout:'nome layout onde vai ser impresso'});
-
+    /*validar dados do form*/
+    resp.redirect('/user/index/2');
 }
 );
 module.exports = routers;
