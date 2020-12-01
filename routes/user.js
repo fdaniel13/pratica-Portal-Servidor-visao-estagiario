775ec8ea3f3494251fae8151b688465495223b31
Fabio Daniel <fdaniel.aborges@gmail.com>
2020-11-30 21:20:22 -0300

    pequena validação(não final)

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const routers = express.Router();
 
+//só pra teste de validação de usuario
+const admin ='admin';
+var id;
 
-routers.get('/index/:idUsuario',(req,resp)=>{
+
+routers.post('/',adminUser,(req,resp)=>{
+    
+    
+    id = Math.floor(Math.random()*1000);
+    /*validar dados do form*/
+    resp.redirect('/user/index/'+id);
+}
+);
+
+routers.get('/index/:id',userId,(req,resp)=>{
+    
     
     //resp.send('pagina inicial');
     //resp.sendFile(__dirname+'/front-end/index.html');
-    resp.render('index',{layout:'main', name: req.params.idUsuario}); //ou resp.render('dadosIniciais',{layout:'nome layout onde vai ser impresso'});
+    resp.render('index',{layout:'main'}); //ou resp.render('dadosIniciais',{layout:'nome layout onde vai ser impresso'});
 
 }
 );
 
-routers.post('/',(req,resp)=>{
-    console.log(req.body);
-    /*validar dados do form*/
-    resp.redirect('/user/index/2');
+function userId(req,resp,next){
+    
+    if(req.params.id==id){
+        next();
+    }else{
+        resp.redirect('/');
+    }
 }
-);
+
+function adminUser(req,resp,next){
+    
+    if(req.body['textUsuario'] === admin && req.body['senhaUsuario'] === admin) {
+         
+         next();
+    }else{
+        resp.redirect('/');
+    }
+
+    
+}
+
 module.exports = routers;
